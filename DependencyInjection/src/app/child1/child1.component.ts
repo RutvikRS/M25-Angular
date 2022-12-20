e05aa4ec35c8a8d00f5a61331ac2d7cafada40a3
RutvikRS <97736308+RutvikRS@users.noreply.github.com>
2022-12-20 20:21:23 +0530

    Add files via upload

---
+++ DependencyInjection/src/app/child1/child1.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { CustomerService } from '../customer.service';
+
+@Component({
+  selector: 'app-child1',
+  templateUrl: './child1.component.html',
+  styleUrls: ['./child1.component.css'],
+  //providers: [CustomerService]
+})
+export class Child1Component {
+  message: string | undefined;
+
+  constructor(private cs:CustomerService){
+  }
+  ngOnInit(): void{
+    this.message=this.cs.display("Techno serve")
+    
+  }
+}
